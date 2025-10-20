4665b08c9b40900ff3ec4e489d3276bad1fba08a
masashiono0611 <masashi.ono.0611@gmail.com>
2025-10-20 09:39:46 +0800

    chore: update deployment scripts to use tsx instead of ts-node

---
+++ hardhat.config.cjs
@@ -0,0 +1,47 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL ?? "";
+const BASE_SEPOLIA_DEPLOYER_PRIVATE_KEY = process.env.BASE_SEPOLIA_DEPLOYER_PRIVATE_KEY ?? "";
+const BASESCAN_API_KEY = process.env.BASESCAN_API_KEY ?? "";
+const OPTIMISM_SEPOLIA_RPC_URL = process.env.OPTIMISM_SEPOLIA_RPC_URL ?? "";
+const OPTIMISM_SEPOLIA_DEPLOYER_PRIVATE_KEY = process.env.OPTIMISM_SEPOLIA_DEPLOYER_PRIVATE_KEY ?? "";
+const OPTIMISM_ETHERSCAN_API_KEY = process.env.OPTIMISM_ETHERSCAN_API_KEY ?? "";
+const ARBITRUM_SEPOLIA_RPC_URL = process.env.ARBITRUM_SEPOLIA_RPC_URL ?? "";
+const ARBITRUM_SEPOLIA_DEPLOYER_PRIVATE_KEY = process.env.ARBITRUM_SEPOLIA_DEPLOYER_PRIVATE_KEY ?? "";
+const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY ?? "";
+
+module.exports = {
+  solidity: {
+    version: "0.8.25",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    baseSepolia: {
+      url: BASE_SEPOLIA_RPC_URL || undefined,
+      accounts: BASE_SEPOLIA_DEPLOYER_PRIVATE_KEY ? [BASE_SEPOLIA_DEPLOYER_PRIVATE_KEY] : undefined,
+    },
+    optimismSepolia: {
+      url: OPTIMISM_SEPOLIA_RPC_URL || undefined,
+      accounts: OPTIMISM_SEPOLIA_DEPLOYER_PRIVATE_KEY ? [OPTIMISM_SEPOLIA_DEPLOYER_PRIVATE_KEY] : undefined,
+    },
+    arbitrumSepolia: {
+      url: ARBITRUM_SEPOLIA_RPC_URL || undefined,
+      accounts: ARBITRUM_SEPOLIA_DEPLOYER_PRIVATE_KEY ? [ARBITRUM_SEPOLIA_DEPLOYER_PRIVATE_KEY] : undefined,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      baseSepolia: BASESCAN_API_KEY,
+      optimismSepolia: OPTIMISM_ETHERSCAN_API_KEY,
+      arbitrumSepolia: ARBISCAN_API_KEY,
+    },
+  },
+};
