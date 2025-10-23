81fca78a8d0769862b32090ca9a2e6de384c241a
Manvendra2000 <mzealpro@gmail.com>
2025-10-24 03:06:23 +0530

    spacing in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { GiftingSplitterArbitrumCard } from "./_components/gifting/GiftingSplitt
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center p-4">
+      <div className="container1">
       <div>
         <h1 className="header">Choose your path</h1>
         <p className="subheader-main">Select where your next impact begins</p>
@@ -14,6 +15,7 @@ export default function Home() {
         <a href="/payroll" className="btn1 btn-primary">Go To Payroll</a>
         <a href="/gifting" className="btn1 btn-primary">Go To Gifting</a>
         <a href="/tools" className="btn1 btn-secondary">Go To Tools</a>
+      </div>
       </div>
     </main>
   );
