8a568202c5705d18e3822ef525ba156c9b9ed41f
Manvendra2000 <mzealpro@gmail.com>
2025-10-23 23:52:00 +0530

    footer stick to bottom always

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Providers>
           <Navbar />
-          {children}
+          <main>
+            {children}
+          </main>
           <Footer />
         </Providers>
       </body>
